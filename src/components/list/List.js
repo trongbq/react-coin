a29e3b9bf09eb8b0c87452d8f0c1d67bbc0be184
161118 <trong.buiquoc@gmail.com>
2018-05-24 17:29:13 +0700

    Request API data

---
+++ src/components/list/List.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import { handleResponse } from '../../helpers';
+import { API_URL } from '../../config';
+import "./Table.css";
+import Loading from "../common/Loading";
+
+class List extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      currencies: [],
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({loading: true});
+
+    fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
+      .then(handleResponse)
+      .then((data) => {
+        this.setState({ currencies: data.currencies, loading: false });
+      })
+      .catch((error) => {
+        this.setState({ error: error.errorMessage, loading: false });
+      });
+  }
+
+  renderChangePercent(percent) {
+    if (percent > 0) {
+      return <span className="percent-raised">{percent}% &uarr;</span>
+    } else if (percent < 0) {
+      return <span className="percent-fallen">{percent}% &darr;</span>
+    } else {
+      return <span>{percent}</span>
+    }
+  }
+
+  render() {
+    const { loading, error, currencies } = this.state;
+
+    if (loading) {
+      return <div className="loading-container"><Loading /></div>
+    }
+
+    if (error) {
+      return <div className="error">{error}</div>
+    }
+
+    return (
+      <div className="Table-container">
+        <table className="Table">
+          <thead className="Table-head">
+            <tr>
+              <th>Cryptocurrency</th>
+              <th>Price</th>
+              <th>Market Cap</th>
+              <th>24H Change</th>
+            </tr>
+          </thead>
+          <tbody className="Table-body">
+            {currencies.map((currency) => (
+              <tr key="currency.id">
+                <td> <span className="Table-rank">{currency.rank}</span>{currency.name}</td>
+                <td><span className="Table-dollar">$ {currency.price}</span></td>
+                <td><span className="Table-dollar">$ {currency.marketCap}</span></td>
+                <td>{this.renderChangePercent(currency.percentChange24h)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './components/common/Header';
+import List from './components/list/List';
 
 import './index.css';
 
@@ -12,7 +13,7 @@ const App = () => {
     <div>
       <Header />
       <h1>{title}</h1>
-      <p>Up to date crypto currencies financial data</p>
+      <List />
     </div>
   );
 }
